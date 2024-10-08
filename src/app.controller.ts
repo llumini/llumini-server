d2d92d8c23b5858e86f3d93678a7abf181362a20
pdro-lucas <pdrolucas.dev@gmail.com>
2024-10-07 21:06:52 -0300

    feat: add createNote endpoint to AppController for note management

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { NotesService } from './notes/note.service';
+
+interface NoteContent {
+  [key: string]: any;
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly notesService: NotesService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Post('note')
+  async createNote(@Body() data: { title: string; content: NoteContent }) {
+    const { title, content } = data;
+
+    return this.notesService.createNote({
+      title,
+      content,
+    });
   }
 }
